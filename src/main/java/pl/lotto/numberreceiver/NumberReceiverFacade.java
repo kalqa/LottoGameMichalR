c8f72b9d2e7c935c0369ebf9c8fe272df2841792
MichalRumiancew <michalr11@gmail.com>
2022-07-20 19:07:17 +0200

    created test shold provider numbers

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -12,14 +12,21 @@ public class NumberReceiverFacade {
                 return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "numbers out of range");
             }
         }
+        if (numbersFromUser.isEmpty() ) {
+            return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "you didn't provide numbers");
+        }
         if (numbersFromUser.size() > 6) {
             return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "to many numbers");
         }
+        if (numbersFromUser.size() < 6) {
+            return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "too few numbers");
+        }
+
+
         Optional<UUID> clientLotteryId = Optional.of(UUID.randomUUID());
-        Optional<LocalDateTime> drawDate = Optional.of(LocalDateTime.now());
+        Optional<LocalDateTime> drawDate = Optional.of(LocalDateTime.of(2022,7,30,12,0));
         return new NumberReceiverResultDto(clientLotteryId, drawDate, "everything is OK");
     }
-
     private boolean isNumberInRange(Integer number) {
         return number >= 1 && number < 100;
     }
