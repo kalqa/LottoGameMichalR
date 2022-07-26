88ce7c59ec424568426e2edacefd32a7b3bc08ab
MichalRumiancew <michalr11@gmail.com>
2022-07-26 18:18:59 +0200

    check

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -1,9 +1,7 @@
 package pl.lotto.numberreceiver;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class NumberReceiverFacade {
     public NumberReceiverResultDto inputNumbers(List<Integer> numbersFromUser) {
@@ -12,6 +10,22 @@ public class NumberReceiverFacade {
                 return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "numbers out of range");
             }
         }
+// set nie będzie zawierał powtarzalnych elementów. ułatwi to sptawe. Przekształć listę w set:
+//            Set<Integer> ExpectedNumbersFromUser = new HashSet<>(numbersFromUser);
+
+  //      public boolean contains(numbersFromUser Integer)
+
+        for (Integer number : numbersFromUser) {
+            if(numbersFromUser.contains(numbersFromUser))  {
+                return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "the numbers are not different");
+            }
+        }
+
+        if (numbersFromUser.contains(numbersFromUser)) {
+            return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "the numbers are not different");
+        }
+
+
         if (numbersFromUser.isEmpty() ) {
             return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "you didn't provide numbers");
         }
@@ -21,9 +35,7 @@ public class NumberReceiverFacade {
         if (numbersFromUser.size() < 6) {
             return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "too few numbers");
         }
-        if (numbersFromUser.size() == 0) {
-            return new NumberReceiverResultDto(Optional.empty(), Optional.empty(), "the numbers are not different");
-        }
+
 
         Optional<UUID> clientLotteryId = Optional.of(UUID.randomUUID());
         Optional<LocalDateTime> drawDate = Optional.of(LocalDateTime.of(2022,7,30,12,0));
@@ -32,4 +44,6 @@ public class NumberReceiverFacade {
     private boolean isNumberInRange(Integer number) {
         return number >= 1 && number < 100;
     }
+
+
 }
